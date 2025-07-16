5d13a9bd044200411fdff9a15eb5eb9516495aee
Kobela Károly <kobi1288@gmail.com>
2025-07-16 09:26:53 +0200

    Protected Routes

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <BrowserRouter>
+  <BrowserRouter>
+    <AuthProvider>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
       </Routes>
-    </BrowserRouter>
+    </AuthProvider>
+  </BrowserRouter>
   );
 }
 
